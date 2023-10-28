1bcbe0d08742987f8e8e5840d7352a4784db6a2f
OlegBarabash <olegbarabash7@gmail.com>
2023-10-28 15:55:49 +0300

    Done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature():number {
+        return this.signature;
+    }
+}
+
+class Person{
+    constructor(private key: Key) { }
+
+    getKey(): Key{
+        return this.key;
+    }
+}
+
+abstract class House{
+    protected door: boolean = false;
+    protected key: Key;
+    private tenants: Person[] = []
+    
+    constructor(key: Key) {
+    this.key = key;
+    }
+    
+    comeIn(person:Person):void {
+        if (this.door) {
+            this.tenants.push(person)
+        }
+     }
+   abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House{
+    openDoor(key: Key): void{
+       let keyKode:number = this.key.getSignature()
+        if (keyKode === key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
